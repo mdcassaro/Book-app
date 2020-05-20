72115531b5a8b0c564bbe83266b6ef18a76abbb1
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-20 13:29:50 -0500

    result conatiner made

---
+++ src/components/ResultContainer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+import ResultCard from"../ResultCard"
+
+
+
+function ResultContainer(){
+    return (
+        <div >
+            <h3>Results</h3>
+            <ResultCard/>
+        </div>
+
+    ) 
+}
+
+export default ResultContainer
