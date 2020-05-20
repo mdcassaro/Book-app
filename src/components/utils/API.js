42d7b0c8ffded52248298163ba88555637871ca0
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-20 13:47:52 -0500

    added google books api key

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -18,3 +18,39 @@ export default {
     return axios.post("/api/books", bookData);
   }
 };
+
+AIzaSyDeDO0-rQIayjNRH7VQKCS7YSjb709ER8U
+
+
+
+import axios from "axios";
+
+const languages = ["JavaScript", "Python", "C", "Ruby", "Java", "PHP", "C#"];
+// Export an object containing methods we'll use for accessing the random user API
+export default {
+  getBooks: function(query) {
+    let queryUrl = "https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=AIzaSyDeDO0-rQIayjNRH7VQKCS7YSjb709ER8U"
+    return new Promise((resolve, reject) => {
+      axios
+        .get(queryUrl)
+        .then(res => {
+          const users = res.data;
+          const results = users.map(user => {
+            return {
+              login: user.login,
+              image: user.avatar_url,
+              language: language
+            };
+          });
+          resolve(results);
+        })
+        .catch(err => reject(err));
+    });
+  },
+  // Return a Promise to simulate an async call
+  getLanguagesList: function() {
+    return new Promise(resolve => {
+      resolve(languages);
+    });
+  }
+};
